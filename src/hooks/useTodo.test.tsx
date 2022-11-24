9cf281d411e787c894a7eab2ffad49ed2c137756
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2022-11-24 09:42:14 +0900

    非同期処理は後回しにする

--- src/hooks/useTodo.test.tsx
+++ src/hooks/useTodo.test.tsx
@@ -20,4 +20,18 @@ describe("useTodo", () => {
         })
         expect(result.current.newTodo).toBe(expectValue)
     })
+    test('todoの追加', () => {
+        const {result} = renderHook(() => useTodo());
+        const expectValue = "テスト";
+        const eventObject = {
+        target: {
+            value: expectValue,
+            },
+            key: "Enter"
+        };
+        act(() => {
+            result.current.addNewTodo(eventObject)
+        })
+        expect(result.current.showTodos).toBe([{id:1,title:expectValue}])
+    })
 });
